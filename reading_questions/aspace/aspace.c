ba0dea563846adc42da90c4aab1959461106a89c
Bonnie Ishiguro <bgishiguro@gmail.com>
2017-02-05 10:15:29 -0500

    aspace.c modifications

--- reading_questions/aspace/aspace.c
+++ reading_questions/aspace/aspace.c
@@ -10,15 +10,25 @@ License: GNU GPLv3
 
 int global;
 
+void print_local()
+{
+	int local = 10;
+	printf ("Address of local is %p\n", &local);
+}
+
 int main ()
 {
     int local = 5;
-    void *p = malloc(128);
+    void *p = malloc(10);
+    void *q = malloc(10);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of global is %p\n", &global);
     printf ("Address of local is %p\n", &local);
     printf ("Address of p is %p\n", p);
+    printf ("Address of q is %p\n", q);
+
+    print_local();
     
     return 0;
 }
