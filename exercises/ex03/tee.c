a97b7c86a70436981d9446e0b9f9e0020da93556
Bonnie Ishiguro <bgishiguro@gmail.com>
2017-02-07 17:09:51 -0500

    tee comments

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -1,15 +1,20 @@
+/*
+A command that reads from stdin and writes to both stdout and another file
+Author: Bonnie Ishiguro
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char mode[1] = "w";
+	char mode[1] = "w"; // the default file access mode is write
 
 	char ch;
 	while ((ch = getopt(argc, argv, "a")) != EOF) {
 		switch (ch) {
 		case 'a':
-			strcpy(mode, "a");
+			strcpy(mode, "a"); // changes the file access mode to append
 			break;
 		default:
 			fprintf(stderr, "Unknown option: '%s'\n", optarg);
@@ -17,14 +22,23 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	argc -= optind;
+	// skip past the options to the command-line arguments
+	argc -= optind; 
 	argv += optind;
 
 	FILE *fp = fopen(argv[0], mode);
 	char input[100];
 
-	fscanf(stdin, "%[^\n]", input);
+	fscanf(stdin, "%[^\n]", input); // read from stdin and store the result in input
 
-	fprintf(fp, "%s\n", input);
-	fprintf(stdout, "%s\n", input);
+	fprintf(fp, "%s\n", input); // print from input to the newly-created file
+	fprintf(stdout, "%s\n", input); // print from input to stdout (the  display)
 }
+
+/* 
+3) Reflect: what worked, what slowed you down?  What would you do
+differently next time?
+
+4) Compare your solution to the real thing.  What do you see in
+professional code that is not in your solution?
+*/
