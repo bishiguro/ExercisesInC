6acb253dbef9b284bda047f0f3794174fc685efc
Bonnie Ishiguro <bgishiguro@gmail.com>
2017-02-07 19:14:19 -0500

    ex03 questions

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -8,13 +8,13 @@ Author: Bonnie Ishiguro
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char mode[2] = "w"; // the default file access mode is write
+	char mode[2] = "w"; // default file access mode is write
 
 	char ch;
 	while ((ch = getopt(argc, argv, "a")) != EOF) {
 		switch (ch) {
 		case 'a':
-			strcpy(mode, "a"); // changes the file access mode to append
+			strcpy(mode, "a"); // changes file access mode to append
 			break;
 		default:
 			fprintf(stderr, "Unknown option: '%s'\n", optarg);
@@ -29,10 +29,10 @@ int main(int argc, char *argv[]) {
 	FILE *fp = fopen(argv[0], mode);
 	char input[100];
 
-	fscanf(stdin, "%[^\n]", input); // read from stdin and store the result in input
+	fscanf(stdin, "%[^\n]", input); // read from stdin
 
-	fprintf(fp, "%s\n", input); // print from input to the newly-created file
-	fprintf(stdout, "%s\n", input); // print from input to stdout (the  display)
+	fprintf(fp, "%s\n", input); // print from input to the file
+	fprintf(stdout, "%s\n", input); // print from input to stdout
 	fclose(fp);
 }
 
@@ -40,6 +40,16 @@ int main(int argc, char *argv[]) {
 3) Reflect: what worked, what slowed you down?  What would you do
 differently next time?
 
+The smaller details slowed me down the most, such as figuring out how to allow 
+scanf to accept text with spaces, how to modify the "mode" string, and the 
+correct placement of lines 26-27.  Overall, however, the HeadFirst C textbook 
+was a good guide for helping me understand using files and accepting command 
+line arguments/options.
+
 4) Compare your solution to the real thing.  What do you see in
 professional code that is not in your solution?
+
+The professional code allows for more than one file, which I did not include.
+It also includes error-checking for opening files.
+
 */
